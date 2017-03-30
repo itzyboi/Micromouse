09358179f5860174e30b8fc8d427c1d14fd7a8a4
itzyboi <luke.itzinger@gmail.com>
2017-03-30 17:23:11 +0100

    -Rewritten functions to now pay attention to both wheels encoders, not just one -Removed braking -New tested values for encoder limits

--- Software/Micromouse/Micromouse.ino
+++ Software/Micromouse/Micromouse.ino
@@ -2,7 +2,7 @@
 // Motors defined whilst looking from rear of robot.
 #define mL 10 // Left motor drive
 #define mR 11 // Right motor drive
-#define enableML 9 // Left motor enable pin (High is on)
+#define enableML 9// Left motor enable pin (High is on)
 #define enableMR 12 // Right motor enable pin (High is on)
 
 #define encoderLB 2 // Left motor encoder blue
@@ -12,8 +12,8 @@
 
 // Constants
 #define squareWidth 500
-#define degrees90C 202
-#define degrees90A 188 
+#define degrees90C 175
+#define degrees90A 202 
 
 
 // Global variables
@@ -24,8 +24,7 @@ volatile int encoderRGCount = 0;
 
 void setup()
 {
-Serial.begin(9600);
-interrupts();
+//Serial.begin(9600);
 pinMode(enableMR, OUTPUT);
 pinMode(enableML, OUTPUT);
 digitalWrite(enableML, LOW); // These go low so early to try to stop robot running away on startup
@@ -38,160 +37,121 @@ pinMode(encoderLG, INPUT);
 pinMode(encoderRB, INPUT);
 pinMode(encoderRG, INPUT);
 
+interrupts();
+
 attachInterrupt(digitalPinToInterrupt(encoderLB), encoderLBCounter, RISING);
 attachInterrupt(digitalPinToInterrupt(encoderLG), encoderLGCounter, RISING);
 attachInterrupt(digitalPinToInterrupt(encoderRB), encoderRBCounter, RISING);
 attachInterrupt(digitalPinToInterrupt(encoderRG), encoderRGCounter, RISING);
 
-//Serial.println("Setup done");
-delay(1000);
-forward(2);
-delay(250);
-antiClockwise90(2);
-delay(250);
-forward(1);
-delay(250);
-antiClockwise90(1);
-delay(250);
-forward(1);
-delay(250);
-clockwise90(1);
-delay(250);
-forward(1);
-delay(250);
-clockwise90(1);
-delay(250);
-forward(1);
-delay(250);
-antiClockwise90(2);
-delay(250);
-forward(1);
-delay(250);
-clockwise90(1);
-delay(250);
-forward(1);
-delay(250);
 }
 
 void loop() 
 {
-
-
+delay(1000);
+forward(1);
+delay(250);
+forward(1);
+delay(250);
+clockwise90(1);
+delay(250);
 }
 
 void forward( int squares)
 {
   //Serial.println("Forward begin");
   // Travels forward x squares, x being int squares.
-  volatile boolean flag = 0;
+
+  boolean flag1 = 1;
+  boolean flag2 = 1;
+  int distance = squareWidth * squares;
+  
+  analogWrite(mL, 195);
+  analogWrite(mR, 191);
   encoderLBCount = 0;
   encoderLGCount = 0;
   encoderRBCount = 0;
   encoderRGCount = 0;
-
-  int distance = squareWidth * squares;
-  
-  analogWrite(mL, 193);
-  analogWrite(mR, 191);
   digitalWrite(enableML, HIGH);
   digitalWrite(enableMR, HIGH);
-  //Serial.println(distance);
 
-  while(!flag)
+  while(flag1 | flag2)
   {
-    flag = (encoderLBCount > distance) || (encoderRBCount > distance);
-    //Serial.println();
+    if(encoderLBCount >= distance)
+      {
+        digitalWrite(enableML, LOW);
+        flag1 = 0;
+      }
+    if(encoderRBCount >= distance)
+      {
+        digitalWrite(enableMR, LOW);
+        flag2 = 0;
+      }
   }
-  
-  // Braking maneuver
-  digitalWrite(enableML, LOW);
-  digitalWrite(enableMR, LOW);
-  //Serial.println("Motors off");
-  //Serial.println("Braking start");
-  //analogWrite(mL, 0);
-  //analogWrite(mR, 0);
-  //Serial.println("Motors on");
-  //digitalWrite(enableML, HIGH);
-  //digitalWrite(enableMR, HIGH);  
-  //delay(1);
-  //digitalWrite(enableML, LOW);
-  //digitalWrite(enableMR, LOW);
-  //Serial.println("Motors off");
-  //Serial.println("Braking end and exit forward");
   return;
 }
 
 void clockwise90( int turns)
 { 
-  volatile boolean flag = 0;
+  boolean flag1 = 1;
+  boolean flag2 = 1;
+
+  int degreestoturn = degrees90C * turns;
+  
+  analogWrite(mL, 168);
+  analogWrite(mR, 95);
   encoderLBCount = 0;
   encoderLGCount = 0;
   encoderRBCount = 0;
   encoderRGCount = 0;
-
-  int degreestoturn = degrees90C * turns;
-  //Serial.println("clockwise start");
-  
-  
-  analogWrite(mL, 161);
-  analogWrite(mR, 95);
   digitalWrite(enableML, HIGH);
   digitalWrite(enableMR, HIGH);
-  //Serial.println(degreestoturn);
   
-  while(!flag)
+  while(flag1 | flag2)
   {
-    flag = (encoderLBCount > degreestoturn || encoderRBCount > degreestoturn);
-    //Serial.println(encoderRBCount);
+    if(encoderLBCount >= degreestoturn)
+      {
+        digitalWrite(enableML, LOW);
+        flag1 = 0;
+      }
+    if(encoderRBCount >= degreestoturn)
+      {
+        digitalWrite(enableMR, LOW);
+        flag2 = 0;
+      }
   }
-
-  // Braking maneuver
-  digitalWrite(enableML, LOW);
-  digitalWrite(enableMR, LOW);
-  //analogWrite(mL, 64);
-  //analogWrite(mR, 191);
-  //digitalWrite(enableML, HIGH);
-  //digitalWrite(enableMR, HIGH);  
-  //delay(1);
-  //digitalWrite(enableML, LOW);
-  //digitalWrite(enableMR, LOW);
   return;
 }
 
 void antiClockwise90( int turns)
 { 
-  volatile boolean flag = 0;
+  boolean flag1 = 1;
+  boolean flag2 = 1;
+
+  int degreestoturn = degrees90C * turns;
+  
+  analogWrite(mL, 95);
+  analogWrite(mR, 168);
   encoderLBCount = 0;
   encoderLGCount = 0;
   encoderRBCount = 0;
   encoderRGCount = 0;
-
-  int degreestoturn = degrees90A * turns;
-  //Serial.println("clockwise start");
-  
-  
-  analogWrite(mL, 95);
-  analogWrite(mR, 161);
   digitalWrite(enableML, HIGH);
   digitalWrite(enableMR, HIGH);
-  //Serial.println(degreestoturn);
   
-  while(!flag)
+  while(flag1 | flag2)
   {
-    flag = (encoderLBCount > degreestoturn || encoderRBCount > degreestoturn);
-    //Serial.println(encoderRBCount);
+    if(encoderLBCount >= degreestoturn)
+      {
+        digitalWrite(enableML, LOW);
+        flag1 = 0;
+      }
+    if(encoderRBCount >= degreestoturn)
+      {
+        digitalWrite(enableMR, LOW);
+        flag2 = 0;
+      }
   }
-
-  // Braking maneuver
-  digitalWrite(enableML, LOW);
-  digitalWrite(enableMR, LOW);
-  //analogWrite(mL, 64);
-  //analogWrite(mR, 191);
-  //digitalWrite(enableML, HIGH);
-  //digitalWrite(enableMR, HIGH);  
-  //delay(1);
-  //digitalWrite(enableML, LOW);
-  //digitalWrite(enableMR, LOW);
   return;
 }
 
