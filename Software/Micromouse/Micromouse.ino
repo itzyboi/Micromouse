cc59b1c36fbf2180ff9fba5ec56b750d2717a7f6
itzyboi <luke.itzinger@gmail.com>
2017-04-03 14:13:16 +0100

    + added for loops to deal with making a maneuver multiple times + added control code to slow motors down when coming up to stop value.(almost eradicated overshoot entirely)

--- Software/Micromouse/Micromouse.ino
+++ Software/Micromouse/Micromouse.ino
@@ -11,9 +11,8 @@
 #define encoderRG 7 // Right motor encoder green
 
 // Constants
-#define squareWidth 500
-#define degrees90C 175
-#define degrees90A 202 
+#define squareWidth 510
+#define degrees90 177
 
 
 // Global variables
@@ -21,176 +20,246 @@ volatile int encoderLBCount = 0;
 volatile int encoderLGCount = 0;
 volatile int encoderRBCount = 0;
 volatile int encoderRGCount = 0;
+volatile boolean comparasonFlag = 1;
 
 void setup()
 {
-//Serial.begin(9600);
-pinMode(enableMR, OUTPUT);
-pinMode(enableML, OUTPUT);
-digitalWrite(enableML, LOW); // These go low so early to try to stop robot running away on startup
-digitalWrite(enableMR, LOW);
-pinMode(mL, OUTPUT);
-pinMode(mR, OUTPUT);
+  Serial.begin(9600);
+  pinMode(enableMR, OUTPUT);
+  pinMode(enableML, OUTPUT);
+  digitalWrite(enableML, LOW); // These go low so early to try to stop robot running away on startup
+  digitalWrite(enableMR, LOW);
+  pinMode(mL, OUTPUT);
+  pinMode(mR, OUTPUT);
 
-pinMode(encoderLB, INPUT);
-pinMode(encoderLG, INPUT);
-pinMode(encoderRB, INPUT);
-pinMode(encoderRG, INPUT);
+  pinMode(encoderLB, INPUT);
+  pinMode(encoderLG, INPUT);
+  pinMode(encoderRB, INPUT);
+  pinMode(encoderRG, INPUT);
 
-interrupts();
+  interrupts();
 
-attachInterrupt(digitalPinToInterrupt(encoderLB), encoderLBCounter, RISING);
-attachInterrupt(digitalPinToInterrupt(encoderLG), encoderLGCounter, RISING);
-attachInterrupt(digitalPinToInterrupt(encoderRB), encoderRBCounter, RISING);
-attachInterrupt(digitalPinToInterrupt(encoderRG), encoderRGCounter, RISING);
+  attachInterrupt(digitalPinToInterrupt(encoderLB), encoderLBCounter, RISING);
+  attachInterrupt(digitalPinToInterrupt(encoderLG), encoderLGCounter, RISING);
+  attachInterrupt(digitalPinToInterrupt(encoderRB), encoderRBCounter, RISING);
+  attachInterrupt(digitalPinToInterrupt(encoderRG), encoderRGCounter, RISING);
 
+
+  delay(2000);
 }
 
-void loop() 
+void loop()
 {
-delay(1000);
-forward(1);
-delay(250);
-forward(1);
-delay(250);
-clockwise90(1);
-delay(250);
+  forward(2);
+  clockwise90(1);
+  forward(2);
+  antiClockwise90(2);
+  forward(2);
+  antiClockwise90(1);
+  forward(2);
+  clockwise90(2);
 }
 
 void forward( int squares)
 {
   //Serial.println("Forward begin");
   // Travels forward x squares, x being int squares.
-
+  byte i = 0;
   boolean flag1 = 1;
   boolean flag2 = 1;
-  int distance = squareWidth * squares;
-  int difference = 0;
-  byte speedL = 195;
-  byte speedR = 191;
-  
-  analogWrite(mL, speedL);
-  analogWrite(mR, speedR);
-  encoderLBCount = 0;
-  encoderLGCount = 0;
-  encoderRBCount = 0;
-  encoderRGCount = 0;
-  digitalWrite(enableML, HIGH);
-  digitalWrite(enableMR, HIGH);
 
-  while(flag1 | flag2)
+  for (i = 0; i < squares; i++)
   {
-    //code goes here
-    difference = encoderLBCount - encoderRBCount;
-    if(difference > 0)
+    analogWrite(mL, 195);
+    analogWrite(mR, 191);
+    encoderLBCount = 0;
+    encoderLGCount = 0;
+    encoderRBCount = 0;
+    encoderRGCount = 0;
+    flag1 = 1;
+    flag2 = 1;
+    digitalWrite(enableML, HIGH);
+    digitalWrite(enableMR, HIGH);
+    while (flag1 | flag2)
+    {
+      //code goes here
+      comparasonFlag = 1;
+      if ((encoderLBCount >= squareWidth) && (flag1 == 1))
       {
-        if(difference > 60)
-          difference = 60;
-        speedL = 195 + difference;
-        digitalWrite(mL, speedL);
+        if (comparasonFlag == 1)
+        {
+          digitalWrite(enableML, LOW);
+          flag1 = 0;
+        }
       }
-    else if(difference < 0)
+      comparasonFlag = 1;
+      if ((encoderRBCount >= squareWidth) && (flag2 == 1))
       {
-        if(difference < -64)
-          difference = -64;
-        speedR = 191 - difference;
-        digitalWrite(mR, speedR);
+        if (comparasonFlag == 1)
+        {
+          digitalWrite(enableMR, LOW);
+          flag2 = 0;
+        }
       }
-    
-    if(encoderLBCount >= distance)
+      comparasonFlag = 1;
+      if ((encoderLBCount >= (squareWidth - 25)) && (flag1 == 1))
       {
-        digitalWrite(enableML, LOW);
-        flag1 = 0;
+        if (comparasonFlag == 1)
+        {
+          analogWrite(mL, 155);
+        }
       }
-    if(encoderRBCount >= distance)
+      comparasonFlag = 1;
+      if ((encoderRBCount >= (squareWidth - 25)) && (flag2 == 1))
       {
-        digitalWrite(enableMR, LOW);
-        flag2 = 0;
+        if (comparasonFlag == 1)
+        {
+          analogWrite(mR, 155);
+        }
       }
+    }
   }
   return;
 }
 
 void clockwise90( int turns)
-{ 
+{
   boolean flag1 = 1;
   boolean flag2 = 1;
+  byte i = 0;
 
-  int degreestoturn = degrees90C * turns;
-  
-  analogWrite(mL, 168);
-  analogWrite(mR, 95);
-  encoderLBCount = 0;
-  encoderLGCount = 0;
-  encoderRBCount = 0;
-  encoderRGCount = 0;
-  digitalWrite(enableML, HIGH);
-  digitalWrite(enableMR, HIGH);
-  
-  while(flag1 | flag2)
+  for (i = 0; i < turns; i++)
   {
-    if(encoderLBCount >= degreestoturn)
+    analogWrite(mL, 168);
+    analogWrite(mR, 95);
+    encoderLBCount = 0;
+    encoderLGCount = 0;
+    encoderRBCount = 0;
+    encoderRGCount = 0;
+    flag1 = 1;
+    flag2 = 1;
+    digitalWrite(enableML, HIGH);
+    digitalWrite(enableMR, HIGH);
+    while (flag1 | flag2)
+    {
+      comparasonFlag = 1;
+      if ((encoderLBCount >= degrees90) && (flag1 == 1))
       {
-        digitalWrite(enableML, LOW);
-        flag1 = 0;
+        if (comparasonFlag == 1)
+        {
+          digitalWrite(enableML, LOW);
+          flag1 = 0;
+        }
       }
-    if(encoderRBCount >= degreestoturn)
+      comparasonFlag = 1;
+      if ((encoderRBCount >= degrees90) && ( flag2 == 1))
       {
-        digitalWrite(enableMR, LOW);
-        flag2 = 0;
+        if (comparasonFlag == 1)
+        {
+          digitalWrite(enableMR, LOW);
+          flag2 = 0;
+        }
       }
+      comparasonFlag = 1;
+      if ((encoderLBCount >= (degrees90 - 25)) && (flag1 == 1))
+      {
+        if (comparasonFlag == 1)
+        {
+          analogWrite(mL, 155);
+        }
+      }
+      comparasonFlag = 1;
+      if ((encoderRBCount >= (degrees90 - 25)) && (flag2 == 1))
+      {
+        if (comparasonFlag == 1)
+        {
+          analogWrite(mR, 99);
+        }
+      }
+    }
   }
   return;
 }
 
 void antiClockwise90( int turns)
-{ 
+{
   boolean flag1 = 1;
   boolean flag2 = 1;
+  byte i = 0;
 
-  int degreestoturn = degrees90C * turns;
-  
-  analogWrite(mL, 95);
-  analogWrite(mR, 168);
-  encoderLBCount = 0;
-  encoderLGCount = 0;
-  encoderRBCount = 0;
-  encoderRGCount = 0;
-  digitalWrite(enableML, HIGH);
-  digitalWrite(enableMR, HIGH);
-  
-  while(flag1 | flag2)
+  for (i = 0; i < turns; i++)
   {
-    if(encoderLBCount >= degreestoturn)
+    analogWrite(mL, 95);
+    analogWrite(mR, 168);
+    encoderLBCount = 0;
+    encoderLGCount = 0;
+    encoderRBCount = 0;
+    encoderRGCount = 0;
+    flag1 = 1;
+    flag2 = 1;
+    digitalWrite(enableML, HIGH);
+    digitalWrite(enableMR, HIGH);
+    while (flag1 | flag2)
+    {
+      comparasonFlag = 1;
+      if ((encoderLBCount >= degrees90) && (flag1 == 1))
       {
-        digitalWrite(enableML, LOW);
-        flag1 = 0;
+        if (comparasonFlag == 1)
+        {
+          digitalWrite(enableML, LOW);
+          flag1 = 0;
+        }
       }
-    if(encoderRBCount >= degreestoturn)
+      comparasonFlag = 1;
+      if ((encoderRBCount >= degrees90) && (flag2 == 1))
       {
-        digitalWrite(enableMR, LOW);
-        flag2 = 0;
+        if (comparasonFlag == 1)
+        {
+          digitalWrite(enableMR, LOW);
+          flag2 = 0;
+        }
       }
+      comparasonFlag = 1;
+      if ((encoderLBCount >= (degrees90 - 25)) && (flag1 == 1))
+      {
+        if (comparasonFlag == 1)
+        {
+          analogWrite(mL, 99);
+        }
+      }
+      comparasonFlag = 1;
+      if ((encoderRBCount >= (degrees90 - 25)) && (flag2 == 1))
+      {
+        if (comparasonFlag == 1)
+        {
+          analogWrite(mR, 155);
+        }
+      }
+    }
   }
   return;
 }
 
 void encoderLBCounter( void )
 {
-encoderLBCount++;
+  encoderLBCount++;
+  comparasonFlag = 0;
 }
 
 void encoderLGCounter( void )
 {
-encoderLGCount++;
+  encoderLGCount++;
+  comparasonFlag = 0;
 }
 
 void encoderRBCounter( void )
 {
-encoderRBCount++;
+  encoderRBCount++;
+  comparasonFlag = 0;
 }
 
 void encoderRGCounter( void )
 {
-encoderRGCount++;
+  encoderRGCount++;
+  comparasonFlag = 0;
 }
